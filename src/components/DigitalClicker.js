acb405029562ff05ed3151b4c11dc9c57abdd227
antimonysayz <antimonysayz@gmail.com>
2020-11-08 01:15:44 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,18 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react';
+
+export default class DigitalClicker extends Component {
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    render(){
+        return(
+            <button></button>
+        )
+    }
+
+}
